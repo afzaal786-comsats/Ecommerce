7b02c19c052fea4ea25d58082784a3b573127f41
Yawar Ali <55242837+yawar-33@users.noreply.github.com>
2021-01-12 03:19:47 +0500

    Directory
    Model index.js

--- controller/customers.js
+++ controller/customers.js
@@ -1,24 +1,22 @@
 // Customers Controller CRUD
 
-const Customer = require('../models').Customers;
+const Customer = require("../models").customer;
 module.exports = {
-
-    async create(req,res) {
-        console.log("Request in Create Cusotmer ", req.body);
-        try {
-            const customerCollection = await Customer
-            .create({
-                customerName : req.body.customerName,
-                contactNumber	: req.body.contactNumber,
-                address	: req.body.address,
-                city	: req.body.city,
-                postalCode	: req.body.postalCode,
-                country: req.body.country,
-            });
-            res.status(200).send(customerCollection);
-        } catch (error) {
-            console.log(error);
-            res.status(400).send(error);
-        }
+  async create(req, res) {
+    console.log("Request in Create Cusotmer ", req.body);
+    try {
+      const customerCollection = await Customer.create({
+        customerName: req.body.customerName,
+        contactNumber: req.body.contactNumber,
+        address: req.body.address,
+        city: req.body.city,
+        postalCode: req.body.postalCode,
+        country: req.body.country,
+      });
+      res.status(200).send(customerCollection);
+    } catch (error) {
+      console.log(error);
+      res.status(400).send(error);
     }
-}
+  },
+};
