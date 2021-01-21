3dac8499cfe32814f3ddfa0aa086a01969af0156
Afzaal <65043912+afzaal786-comsats@users.noreply.github.com>
2021-01-21 22:09:37 +0500

    Removing Previous Data From main
    Removing Previous Data From main

--- models/category.js
+++
@@ -1,9 +0,0 @@
-// Categories Schema 
-
-module.exports=(sequelize,DataTypes)=>{
-    let Categories = sequelize.define('Categories',{
-        categoryName : DataTypes.STRING,
-        description	: DataTypes.STRING,
-    });
-    return Categories;
-}
